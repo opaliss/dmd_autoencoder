04d1250994a3ec504fdc5bab7d2d5502455a5b49
Opal Issan <oissan@predsci.com>
2021-01-19 10:27:06 -0800

    edit auto-encoder and remove batch size in order to input a different testing dim. (ex: rings)

--- dmd_machine/autoencoder_network.py
+++ dmd_machine/autoencoder_network.py
@@ -9,36 +9,41 @@ class Coder(keras.Model):
         super(Coder, self).__init__(**kwargs)
 
         # Build out the encoder network.
-        self.encoder = keras.Sequential()
-        self.encoder.add(Input(shape=(hyp_params['phys_dim'],), batch_size=hyp_params['batch_size']))
+        self.encoder = keras.Sequential(name="encoder")
+        self.encoder.add(Input(shape=(hyp_params['phys_dim'],)))
 
-        for _ in range(hyp_params['num_en_layers']):
+        for ii in range(hyp_params['num_en_layers']):
             self.encoder.add(Dense(hyp_params['num_en_neurons'],
                                    activation=hyp_params['activation'],
                                    kernel_initializer=hyp_params['weight_initializer'],
                                    kernel_regularizer=tf.keras.regularizers.l1(0.01),
-                                   bias_initializer=hyp_params['bias_initializer']))
+                                   bias_initializer=hyp_params['bias_initializer'],
+                                   trainable=True, name='enc_' + str(ii)))
+
         self.encoder.add(Dense(hyp_params['latent_dim'],
                                activation="linear",
                                kernel_regularizer=tf.keras.regularizers.l1(0.01),
                                kernel_initializer=hyp_params['weight_initializer'],
-                               bias_initializer=hyp_params['bias_initializer']))
+                               bias_initializer=hyp_params['bias_initializer'],
+                               trainable=True, name='enc_out'))
 
         # Build out the decoder network.
-        self.decoder = keras.Sequential()
-        self.decoder.add(Input(shape=(hyp_params['latent_dim'],), batch_size=hyp_params['batch_size']))
-        for _ in range(hyp_params['num_en_layers']):
+        self.decoder = keras.Sequential(name="decoder")
+        self.decoder.add(Input(shape=(hyp_params['latent_dim'],)))
+        for ii in range(hyp_params['num_en_layers']):
             self.decoder.add(Dense(hyp_params['num_en_neurons'],
                                    activation=hyp_params['activation'],
                                    kernel_initializer=hyp_params['weight_initializer'],
                                    kernel_regularizer=tf.keras.regularizers.l1(0.01),
-                                   bias_initializer=hyp_params['bias_initializer']))
+                                   bias_initializer=hyp_params['bias_initializer'],
+                                   trainable=True, name='dec_' + str(ii)))
 
         self.decoder.add(Dense(hyp_params['phys_dim'],
                                activation="linear",
                                kernel_initializer=hyp_params['weight_initializer'],
                                kernel_regularizer=tf.keras.regularizers.l1(0.01),
-                               bias_initializer=hyp_params['bias_initializer']))
+                               bias_initializer=hyp_params['bias_initializer'],
+                               trainable=True, name='dec_out'))
 
     def call(self, x):
         y = self.encode(x)
@@ -58,26 +63,3 @@ class Coder(keras.Model):
         for tstep in range(y.shape[-1]):
             x.append(self.decoder(y[:, :, tstep]))
         return tf.transpose(x, perm=[1, 2, 0])
-
-    # @tf.function
-    # def encode(self, x):
-    #     y = tf.Variable(tf.zeros(
-    #         shape=(self.batch_size, self.latent_dim, self.num_t_steps), dtype=tf.float32))
-    #     for tstep in range(self.num_t_steps):
-    #         y[:, :, tstep].assign(self.encoder(x[:, :, tstep]))
-    #     return y
-    #
-    # @tf.function
-    # def decode(self, y):
-    #     x_ae = tf.Variable(tf.zeros(
-    #         shape=(self.batch_size, self.latent_dim, self.num_t_steps), dtype=tf.float32))
-    #     for tstep in range(self.num_t_steps):
-    #         x_ae[:, :, tstep].assign(self.decoder(y[:, :, tstep]))
-    #     return x_ae
-    #
-    # def decode_predict(self, y_pred):
-    #     y_pred_dec = tf.Variable(tf.zeros(
-    #         shape=(self.batch_size, self.latent_dim, self.num_t_steps), dtype=tf.float32))
-    #     for tstep in range(self.num_t_steps):
-    #         self.y_pred_dec[:, :, tstep].assign(self.decoder(y_pred[:, :, tstep]))
-    #     return y_pred_dec
